c7490bec8f189f118ad7b953d1e547136cfef8e8
Moshe Wainner <Moshewiner13@gmail.com>
2020-03-16 12:51:15 +0200

    trying to cleanup

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -3,38 +3,12 @@ import { messageSchemeType } from './message.scheme';
 import { WordCountAggregator } from '../data-aggregators/word-count';
 import { MediaMessageFilter } from '../messages-filters/media.filter';
 import { SystemAuthorMessageFilter } from '../messages-filters/system-author.filter';
-import { WordCountType } from './word-count.scheme';
+import { wordCountfields } from './word-count.scheme';
 
 const queryType = new graphql.GraphQLObjectType({
     name: 'AggregationsQuery',
     fields: {
-        wordCount: {
-            type: graphql.GraphQLList(WordCountType),
-            args: {
-                author: { type: graphql.GraphQLString },
-            },
-            resolve: (source, args, context, info: graphql.GraphQLResolveInfo, ) => {
-                const parser = new WordCountAggregator([
-                    new MediaMessageFilter(),
-                    new SystemAuthorMessageFilter()
-                ]);
-                const aggregationResults = parser.aggregate(context.conversation);
-                return Object.keys(aggregationResults).reduce((results, author) => {
-                    results.push({
-                        author,
-                        keywords: Object.keys(aggregationResults[author])
-                            .reduce((keywords, keyword) => {
-                                keywords.push({
-                                    keyword,
-                                    count: aggregationResults[author][keyword]
-                                });
-                                return keywords;
-                            }, [])
-                    });
-                    return results;
-                }, []);
-            }
-        }
+        ...wordCountfields
     }
 });
 
--- src/schemas/word-count.scheme.ts
+++ src/schemas/word-count.scheme.ts
@@ -1,6 +1,14 @@
 import * as graphql from "graphql";
+import { WordCountAggregator } from "../data-aggregators/word-count";
+import { MediaMessageFilter } from "../messages-filters/media.filter";
+import { SystemAuthorMessageFilter } from "../messages-filters/system-author.filter";
 
-export const WordCountType = new graphql.GraphQLObjectType({
+const parser = new WordCountAggregator([
+    new MediaMessageFilter(),
+    new SystemAuthorMessageFilter()
+]);
+
+const WordCountType = new graphql.GraphQLObjectType({
     name: 'WordCountType',
     fields: {
         author: { type: graphql.GraphQLString },
@@ -15,3 +23,33 @@ export const WordCountType = new graphql.GraphQLObjectType({
         }
     }
 });
+
+const resolver = (source, args, context, info: graphql.GraphQLResolveInfo, ) => {
+    const aggregationResults = parser.aggregate(context.conversation);
+
+    return Object.keys(aggregationResults).reduce((results, author) => {
+        results.push({
+            author,
+            keywords: Object.keys(aggregationResults[author])
+                .reduce((keywords, keyword) => {
+                    keywords.push({
+                        keyword,
+                        count: aggregationResults[author][keyword]
+                    });
+                    return keywords;
+                }, [])
+        });
+        return results;
+    }, []);
+};
+
+
+export const wordCountfields = {
+    wordCount: {
+        type: graphql.GraphQLList(WordCountType),
+        args: {
+            author: { type: graphql.GraphQLString },
+        },
+        resolve: resolver
+    }
+};
